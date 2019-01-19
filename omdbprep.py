be20d487a874aa90415fc1bda98419fedf043547
gervisc <gvisscher@gmail.com>
2019-01-19 23:57:47 +0100

    start omdb p2

--- omdbprep.py
+++ omdbprep.py
@@ -1,12 +1,44 @@
-
+import json
 import csv
 
 
 from omdbget import omdbget
 
-print(omdbget("tt3747978"))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+def omdbprep(movies):
+    i=0
+    for movie in movies:
+        i+=1
+        if i== 10:
+            break
+        resp = omdbget(movie[0])
+        item = resp.json()
+        country = item["Country"]
+        print(country)
+        awards = item["Awards"]
+        oscarindex = awards.find("Oscar")
+        if(oscarindex>0):
+            oscar = int(awards[max(oscarindex-3,0):oscarindex])
+            print(oscar)
+        winindex = awards.find("win")
+        if(winindex>0):
+            if(awards[max(winindex-5,0)] == " "):
+                wins = int(awards[max(winindex - 4, 0):winindex])
+            else:
+                wins = int(awards[max(winindex-3,0):winindex])
+            print(wins)
+        nominationindex = awards.find("nomination")
+        if(nominationindex>0):
+            if (awards[max(nominationindex - 5, 0)] == " "):
+                nominations = int(awards[max(nominationindex - 4, 0):nominationindex])
+            else:
+                nominations = int(awards[max(nominationindex - 3, 0):nominationindex])
+            print(nominations)
+        print(awards)
+        actors = item["Actors"]
+        print(actors)
+        rated = item["Rated"]
+        print(rated)
+
 
 
 #
